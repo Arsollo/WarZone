70252a1b82ded02121f84a21073d17fe4497a588
Hamzah Muhammad <99919629+hamzahmhd@users.noreply.github.com>
2023-04-16 15:44:30 -0400

    w/ ps

---
+++ PlayerStrategy.cpp
@@ -0,0 +1,89 @@
+#include "PlayerStrategy.h"
+
+// Human Player Strategy
+void HumanPlayerStrategy::issueOrder()
+{
+
+}
+
+vector<Territory*> HumanPlayerStrategy::toAttack()
+{
+
+}
+
+vector<Territory*> HumanPlayerStrategy::toDefend()
+{
+
+}
+
+
+
+// Aggressive Player Strategy
+void AggressivePlayerStrategy::issueOrder()
+{
+
+}
+
+vector<Territory*> AggressivePlayerStrategy::toAttack()
+{
+
+}
+
+vector<Territory*> AggressivePlayerStrategy::toDefend()
+{
+
+}
+
+
+
+// Benevolent Player Strategy
+void BenevolentPlayerStrategy::issueOrder()
+{
+
+}
+
+vector<Territory*> BenevolentPlayerStrategy::toAttack()
+{
+
+}
+
+vector<Territory*> BenevolentPlayerStrategy::toDefend()
+{
+
+}
+
+
+// Neutral Player Strategy
+void NeutralPlayerStrategy::issueOrder()
+{
+
+}
+
+vector<Territory*> NeutralPlayerStrategy::toAttack()
+{
+
+}
+
+vector<Territory*> NeutralPlayerStrategy::toDefend()
+{
+
+}
+
+
+
+
+// Cheater Player Strategy
+void CheaterPlayerStrategy::issueOrder()
+{
+
+}
+
+vector<Territory*> CheaterPlayerStrategy::toAttack()
+{
+
+}
+
+vector<Territory*> CheaterPlayerStrategy::toDefend()
+{
+
+}
